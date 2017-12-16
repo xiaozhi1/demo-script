4ccfe8fc5fbfa2e34a38bab2e5712565fbe8f7ac
xiaozhi1 <qasxed5@163.com>
2017-12-16 22:20:20 +0800

    bash脚本和node脚本

---
+++ demo.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+
+var dirName = process.argv[2]
+
+process.chdir("/Users/frank/Desktop")
+fs.mkdirSync("./" + dirName)
+process.chdir("./" + dirName)
+fs.mkdirSync('css')
+fs.mkdirSync('js')
+
+fs.writeFileSync("./index.html", "<!DOCTYPE><title>Hello</title><h1>Hi</h1>")
+fs.writeFileSync("css/style.css", "h1{color: red;}")
+fs.writeFileSync("./js/main.js", "var string = 'Hello World'alert(string)")
+
+process.exit(0)
