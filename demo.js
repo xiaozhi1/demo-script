2e67b6062a704e5ae0069884a7d2ba76297751e8
xiaozhi1 <qasxed5@163.com>
2017-12-16 22:30:05 +0800

    添加换行

--- demo.js
+++ demo.js
@@ -10,8 +10,8 @@ process.chdir("./" + dirName)
 fs.mkdirSync('css')
 fs.mkdirSync('js')
 
-fs.writeFileSync("./index.html", "<!DOCTYPE><title>Hello</title><h1>Hi</h1>")
+fs.writeFileSync("./index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
 fs.writeFileSync("css/style.css", "h1{color: red;}")
-fs.writeFileSync("./js/main.js", "var string = 'Hello World'alert(string)")
+fs.writeFileSync("./js/main.js", "var string = 'Hello World\nalert(string)")
 
 process.exit(0)
